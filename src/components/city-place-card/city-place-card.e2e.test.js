4e79b283bcc274be0a54e9f7d89bc6e35a95d684
Salamatov <dimasalam@yandex.ru>
2020-03-01 06:44:15 +0300

    module5-task2 e2e test

--- src/components/city-place-card/city-place-card.e2e.test.js
+++ src/components/city-place-card/city-place-card.e2e.test.js
@@ -1,6 +1,10 @@
 import React from "react";
 import {shallow} from "enzyme";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
 import CityPlaceCard from "./city-place-card";
+
+const mockStore = configureStore([]);
 
 const placeCardInfo = {
   id: 1,
@@ -12,15 +16,71 @@ const placeCardInfo = {
   raiting: 4.7
 };
 
+const cities = [
+  {
+    id: 17,
+    name: `Амстердам`,
+    cord: [52.38333, 4.9]
+  },
+  {
+    id: 15,
+    name: `Барнаул`,
+    cord: [53.346785, 83.776856]
+  },
+  {
+    id: 16,
+    name: `Новосибирск`,
+    cord: []
+  }
+];
+
+const offers = [
+  {
+    id: 1,
+    title: `Комната №1`,
+    isPremium: false,
+    image: `https://upload.wikimedia.org/wikipedia/commons/3/34/Nanteos_%28geograph_3153536%29.jpg`,
+    costPerNignt: 80,
+    type: `room`,
+    raiting: 4.7
+  },
+  {
+    id: 2,
+    title: `Комната №115`,
+    isPremium: false,
+    image: `https://upload.wikimedia.org/wikipedia/commons/3/34/Nanteos_%28geograph_3153536%29.jpg`,
+    costPerNignt: 60,
+    type: `apartment`,
+    raiting: 2
+  },
+];
+
+const city = {
+  id: 17,
+  name: `Амстердам`,
+  cord: [52.38333, 4.9]
+};
+
+const store = mockStore({
+  citiesList: cities,
+  placesInCity: offers,
+  city
+});
+
 describe(`CityPlaceCard e2e`, () => {
 
   const onMouseEnter = jest.fn();
   const handleClickCardTitle = jest.fn();
-  const cityPlaceCard = shallow(<CityPlaceCard
-    info={placeCardInfo}
-    cardClass="cities__place-"
-    onCityPlaceCardMouseEnter={onMouseEnter}
-    onClickCardTitle={handleClickCardTitle} />);
+  const cityPlaceCard = shallow(
+      <Provider store={store}>
+        <CityPlaceCard
+          info={placeCardInfo}
+          cardClass="cities__place-"
+          onArticleMouseEnter={onMouseEnter}
+          onArticleMouseLeave={()=>{}}
+          onClickCardTitle={handleClickCardTitle} />
+      </Provider>
+  );
 
   it(`Should mouse over on card and card data should come into callback`, () => {
     cityPlaceCard.simulate(`mouseEnter`);
