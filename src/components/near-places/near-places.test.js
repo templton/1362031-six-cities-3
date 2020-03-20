67eac0683e28d2afe94aecdd892b92b2b8c54ba6
Salamatov <dimasalam@yandex.ru>
2020-03-20 09:56:39 +0300

    module8-task2 как протетисровать near-places

--- src/components/near-places/near-places.test.js
+++ src/components/near-places/near-places.test.js
@@ -2,11 +2,16 @@ import React from "react";
 import renderer from "react-test-renderer";
 import {Provider} from "react-redux";
 import configureStore from "redux-mock-store";
-import NearPlaces from "./near-places";
+import {NearPlaces} from "./near-places";
+import StoreNameSpace from "../../store/store-name-space";
+import {selectMouseOverPlaceCard} from "../../store/filters/selectors";
+import {Router} from "react-router-dom";
+import history from "../../history";
+
 
 const mockStore = configureStore([]);
 
-const defaultCity = [52.38333, 4.9];
+const cityCord = [52.38333, 4.9];
 
 const neighbourhoodPlaces = [
   {
@@ -41,6 +46,30 @@ const neighbourhoodPlaces = [
   }
 ];
 
+const places = [{
+  "id": 1,
+  "city":
+    {
+      name: `Amsterdam`,
+      location: {latitude: 52.37454, longitude: 4.897976, zoom: 13},
+      id: `Amsterdam`
+    },
+  "image": `https://htmlacademy-react-3.appspot.com/six-cities/static/hotel/17.jpg`,
+  "images": [{id: 0, src: `https://htmlacademy-react-3.appspot.com/six-cities/static/hotel/16.jpg`}],
+  "title": `Tile House`,
+  "isPremium": false,
+  "isFavorite": false,
+  "rating": 4.2,
+  "features": [`Air conditioning`, `Washer`, `Breakfast`, `Baby seat`, `Laptop friendly workspace`],
+  "costPerNignt": 293,
+  "type": `room`,
+  "countBedrooms": 1,
+  "countGuest": 1,
+  "description": `A new spacious villa, one floor. All commodities, jacuzzi and beautiful scenery. Ideal for families or friends.`,
+  "cord": [52.37854, 4.894976], "intoTitle": `intoTitle ???`,
+  "owner": {name: `Angelina`, avatar: `img/avatar-angelina.jpg`, isPro: true}
+}];
+
 const city = {
   id: 17,
   name: `Амстердам`,
@@ -66,6 +95,9 @@ const cities = [
 ];
 
 const store = mockStore({
+  [StoreNameSpace.FILTERS]: {
+    mouseOverPlaceCard: {}
+  },
   citiesList: cities,
   city
 });
@@ -75,15 +107,11 @@ describe(`Render near places`, () => {
     const tree = renderer
       .create(
           <Provider store={store}>
-            <NearPlaces
-              neighbourhoodPlaces={neighbourhoodPlaces}
-              onClickCardTitle={()=>{}}
-              cityCord={defaultCity}/>
-          </Provider>, {
-            createNodeMock: () => {
-              return document.createElement(`div`);
-            }
-          }).toJSON();
+            <Router history={history}>
+              <NearPlaces cityCord={cityCord} places={places}/>
+            </Router>
+          </Provider>
+      ).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
