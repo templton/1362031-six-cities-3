50d87b44e835026569a180c0aea23c333e441d05
Salamatov <dimasalam@yandex.ru>
2020-03-20 05:55:09 +0300

    module8-task2 пустая страница для избранного

--- src/components/favorites-cards/favorites-cards.jsx
+++ src/components/favorites-cards/favorites-cards.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from "react";
+import React, {PureComponent, Fragment} from "react";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import {selectFavourites} from "../../store/favourites/selectors";
@@ -19,6 +19,30 @@ class FavoritesCards extends PureComponent {
     const sities = Array.from(new Set(hotels.map((item)=>{
       return item.city.name;
     })));
+
+    if (sities.length === 0){
+      return (
+        <Fragment>
+          <main className="page__main page__main--favorites page__main--favorites-empty">
+            <div className="page__favorites-container container">
+              <section className="favorites favorites--empty">
+                <h1 className="visually-hidden">Favorites (empty)</h1>
+                <div className="favorites__status-wrapper">
+                  <b className="favorites__status">Nothing yet saved.</b>
+                  <p className="favorites__status-description">Save properties to narrow down search or plan yor future
+                    trips.</p>
+                </div>
+              </section>
+            </div>
+          </main>
+          <footer className="footer">
+            <a className="footer__logo-link" href="main.html">
+              <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
+            </a>
+          </footer>
+        </Fragment>
+      );
+    }
 
     return (
       <main className="page__main page__main--favorites">
